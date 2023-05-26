639e4d9280ff66a5b72b26363ce8c437bb1d03b0
Tom Kubasik <tom.kubasik@lunabee.com>
2023-05-26 17:11:08 +0200

    fix status bar color

--- androidApp/src/main/java/com/lunabee/leaguewiki/android/MainActivity.kt
+++ androidApp/src/main/java/com/lunabee/leaguewiki/android/MainActivity.kt
@@ -3,7 +3,14 @@ package com.lunabee.leaguewiki.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.lunabee.leaguewiki.android.common.MainNavGraph
 import com.lunabee.leaguewiki.android.theme.LeagueWikiTheme
 
@@ -12,7 +19,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
+            val systemUiController = rememberSystemUiController()
+            systemUiController.setSystemBarsColor(
+                color = Color.Transparent,
+                isNavigationBarContrastEnforced = false,
+                darkIcons = !isSystemInDarkTheme(),
+            )
+
             LeagueWikiTheme {
+                val backgroundColor = LeagueWikiTheme.colors.background
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .drawBehind { drawRect(color = backgroundColor) })
                 MainNavGraph()
             }
         }
